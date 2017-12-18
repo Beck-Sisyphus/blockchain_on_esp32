0010993597d87d81766bddd9c3f55148ea01f57b
Beck <pshfls@gmail.com>
2017-12-18 17:11:37 +0800

    local storage before converting to components

--- main/blockchain_main.c
+++ main/blockchain_main.c
@@ -7,15 +7,26 @@
 #include "stdint.h"
 #include "stdio.h"
 #include "string.h"
+#include "stdbool.h"
 
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
 #include "esp_log.h"
+#include "esp_ota_ops.h"
 
 #include "blockchain.h"
 #include "mbedtls/sha256.h"
 #include <time.h>
+#include <esp_partition.h>
 #include "cJSON.h"
+
+#include "nvs.h"
+#include "nvs_flash.h"
+
+/**
+ * Global cJSON struct for blockchain
+ */
+cJSON *chainRoot;
 
 static const char *TAG = "blockchain";
 
@@ -68,16 +79,16 @@ uint8_t *alloc_hex(const char *str, size_t *len)
  * @param prevBlock
  * @param nextBlock
  */
-static void calculateHash(block_t* prevBlock, block_t* nextBlock)
+static void calculateHashBlock(block_t *block, uint8_t *dst)
 {
     // index + previousHash + timestamp + dataHash, 1 + 32 + 8 + 32 = 73
     uint8_t input[73];
-    * input                     = prevBlock->index;
-    *(input + 1)                = prevBlock->hash[0];
-    *(uint64_t *)(input + 33)   = prevBlock->timestamp;
-    *(input + 41)               = prevBlock->dataHash[0];
+    * input                     = block->index;
+    *(input + 1)                = block->previousHash[0];
+    *(input + 33)   = block->timestamp[0];
+    *(input + 41)               = block->dataHash[0];
 
-    mbedtls_sha256(input, 73, nextBlock->hash, 0);
+    mbedtls_sha256(input, 73, dst, 0);
 }
 
 static void generateBlock(block_t *prevBlock, block_t *block, uint8_t* datahash)
@@ -86,22 +97,23 @@ static void generateBlock(block_t *prevBlock, block_t *block, uint8_t* datahash)
     time(&now);
 //    block_t preBlock = getLatestBlock();
     block->index = prevBlock->index + (uint8_t)1U;
-    block->timestamp = (uint64_t)now;
+    block->timestamp = malloc(8);
+    *(uint64_t *)block->timestamp = (uint64_t)now;
     block->dataHash = malloc(32);
     block->previousHash = malloc(32);
     block->hash = malloc(32);
 
 //    memcpy(block->dataHash, datahash,  (size_t) 32);
     memcpy(block->previousHash, prevBlock->hash, (size_t) 32);
-    calculateHash(prevBlock, block);
 }
 
-static void generateGenesis(block_t* genesis, const char* hash_value)
+static void generateGenesis(block_t *genesis, const char* hash_value)
 {
     time_t now;
     time(&now);
     genesis->index = 0;
-    genesis->timestamp = (uint64_t)now;
+    genesis->timestamp = malloc(8);
+    *(uint64_t *)genesis->timestamp = (uint64_t)now;
     genesis->dataHash = malloc(32);
     genesis->previousHash = malloc(32);
     genesis->hash = malloc(32);
@@ -113,60 +125,110 @@ static void generateGenesis(block_t* genesis, const char* hash_value)
     free(temp_hash);
 }
 
+static void freeBlock(block_t *blockToFree)
+{
+    free(blockToFree->dataHash);
+    free(blockToFree->previousHash);
+    free(blockToFree->hash);
+    free(blockToFree);
+}
+
+static bool isValidNewBlock(block_t* prevBlock, block_t *newBlock)
+{
+    uint8_t verifyHash[32];
+    calculateHashBlock(newBlock, verifyHash);
+    if (prevBlock->index + 1 != newBlock->index)
+    {
+        ESP_LOGI(TAG, "block index error");
+        return false;
+    } else if (prevBlock->hash != newBlock->previousHash)
+    {
+        ESP_LOGI(TAG, "new block previous hash is not equal to old block hash");
+        return false;
+    } else if (verifyHash != newBlock->hash)
+    {
+        ESP_LOGI(TAG, "invalid new block hash, block corrupted.");
+        return false;
+    }
+    return true;
+}
+
+/**
+ * Add single block the the main cJSON root
+ * @param block_to_add
+ */
+static void addBlockToJSONRoot(block_t *block_to_add)
+{
+    cJSON *newBlock;
+    cJSON_AddItemToObject(chainRoot, "genesis block", newBlock = cJSON_CreateObject());
+    cJSON_AddNumberToObject(newBlock, "index",        block_to_add->index);
+    cJSON_AddStringToObject(newBlock, "timestamp",    (const char *)block_to_add->timestamp);
+    cJSON_AddStringToObject(newBlock, "dataHash",     (const char *)block_to_add->dataHash);
+    cJSON_AddStringToObject(newBlock, "previousHash", (const char *)block_to_add->previousHash);
+    cJSON_AddStringToObject(newBlock, "currentHash",  (const char *)block_to_add->hash);
+}
+
+/**
+* Hash the partition data, done
+* TODO: solve the memory size issues
+*/
+static void hashRunningPatitionToBlock(block_t *block)
+{
+    const esp_partition_t *running = esp_ota_get_running_partition();
+    uint8_t *partition_data = (uint8_t *) malloc((size_t)(running->size/10));
+    esp_partition_read(running, 0, partition_data, (size_t)(running->size/10));
+    mbedtls_sha256(partition_data, (size_t)(running->size/10), block->dataHash, 0);
+    free(partition_data);
+}
+
 static void create_block_task(void *pvParameter)
 {
-    block_t genesisBlock, nextBlock;
+    chainRoot = cJSON_CreateObject();
+
+    block_t *genesisBlock = malloc(sizeof(block_t));
+    block_t *nextBlock = malloc(sizeof(block_t));
 
     const char *genesisHash = "816534932c2b7154836da6afc367695e6337db8a921823784c14378abed4f7d7";
-    generateGenesis(&genesisBlock, genesisHash);
+    generateGenesis(genesisBlock, genesisHash);
 
-    generateBlock(&genesisBlock, &nextBlock, 0x0);
+    generateBlock(genesisBlock, nextBlock, 0x0);
+
+    addBlockToJSONRoot(genesisBlock);
+
+    char *blockInJSON = cJSON_PrintUnformatted(chainRoot);
+
+    printf("%s \n", blockInJSON);
+    cJSON_Delete(chainRoot);
+
+    hashRunningPatitionToBlock(nextBlock);
 
     /**
-     * parse to cJSON
+     * Rehash the new block
      */
-    cJSON *root = NULL;
-    cJSON *block1, *block2;
-    root = cJSON_CreateObject();
-    cJSON_AddItemToObject(root, "genesis block", block1 = cJSON_CreateObject());
-    cJSON_AddNumberToObject(block1, "index",        genesisBlock.index);
-    cJSON_AddNumberToObject(block1, "timestamp",    genesisBlock.timestamp);
-    cJSON_AddStringToObject(block1, "dataHash",     (const char *)genesisBlock.dataHash);
-    cJSON_AddStringToObject(block1, "previousHash", (const char *)genesisBlock.previousHash);
-    cJSON_AddStringToObject(block1, "currentHash",  (const char *)genesisBlock.hash);
-    cJSON_AddItemToObject(root, "second block", block2 = cJSON_CreateObject());
-    cJSON_AddNumberToObject(block2, "index",        nextBlock.index);
-    cJSON_AddNumberToObject(block2, "timestamp",    nextBlock.timestamp);
-    cJSON_AddStringToObject(block2, "dataHash",     (const char *)nextBlock.dataHash);
-    cJSON_AddStringToObject(block2, "previousHash", (const char *)nextBlock.previousHash);
-    cJSON_AddStringToObject(block2, "currentHash",  (const char *)nextBlock.hash);
+    calculateHashBlock(nextBlock, nextBlock->hash);
+    addBlockToJSONRoot(nextBlock);
 
-    char* print_out = cJSON_Print(root);
-//    char* print_out = cJSON_PrintUnformatted(root);
-    printf("%s \n", print_out);
-    cJSON_Delete(root);
+    isValidNewBlock(genesisBlock, nextBlock);
 
     /**
      * Debug process
      */
-    print_hex("start time", (uint8_t *)&genesisBlock.timestamp, 8);
-    print_hex("data hash", genesisBlock.dataHash, 32);
-    print_hex("genesis prev hash", genesisBlock.previousHash, 32);
-    print_hex("genesis block hash", genesisBlock.hash, 32);
-    print_hex("next data hash", nextBlock.dataHash, 32);
-    print_hex("next prev hash", nextBlock.previousHash, 32);
-    print_hex("next block hash", nextBlock.hash, 32);
+    print_hex("start time", genesisBlock->timestamp, 8);
+    print_hex("data hash", genesisBlock->dataHash, 32);
+    print_hex("genesis prev hash", genesisBlock->previousHash, 32);
+    print_hex("genesis block hash", genesisBlock->hash, 32);
+    print_hex("next data hash", nextBlock->dataHash, 32);
+    print_hex("next prev hash", nextBlock->previousHash, 32);
+    print_hex("next block hash", nextBlock->hash, 32);
 
-    free(genesisBlock.dataHash);
-    free(genesisBlock.previousHash);
-    free(genesisBlock.hash);
+    freeBlock(genesisBlock);
+    freeBlock(nextBlock);
+    genesisBlock = NULL;
+    nextBlock = NULL;
 
-    free(nextBlock.dataHash);
-    free(nextBlock.previousHash);
-    free(nextBlock.hash);
     vTaskDelay(10000 / portTICK_PERIOD_MS);
 
-    printf("Restarting now.\n");
+    ESP_LOGI(TAG, "Restarting now.\n");
     fflush(stdout);
 
     esp_restart();
